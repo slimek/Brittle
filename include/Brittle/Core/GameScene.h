1130b4da7f72d6ef67bc388cf52bdd2027a524bd
slimek <slime573@gmail.com>
2014-04-08 00:48:43 +0800

    Add GameScene.h

---
+++ include/Brittle/Core/GameScene.h
@@ -0,0 +1,28 @@
+// Brittle C++ Library - Core - Game Scene Header
+
+#ifndef __BRITTLE_CORE_GAME_SCENE_H
+#define __BRITTLE_CORE_GAME_SCENE_H
+#pragma once
+
+#include <Brittle/Setup/BrittleDefs.h>
+
+
+namespace Brittle
+{
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Game Scene
+//
+
+class GameScene : public Scene
+{
+};
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+} // namespace Brittle
+
+#endif // __BRITTLE_CORE_GAME_SCENE_H
