20c63d7b938f1ab7b228e58275312acd91c03d8b
slimek <slime573@gmail.com>
2014-05-01 16:00:36 +0800

    Add UiTypes.h .

---
+++ include/Brittle/Ui/UiTypes.h
@@ -0,0 +1,27 @@
+// Brittle C++ Library - UI - UI Basic Types Header
+
+#ifndef __BRITTLE_UI_UI_TYPES_H
+#define __BRITTLE_UI_UI_TYPES_H
+#pragma once
+
+#include <Brittle/Setup/BrittleDefs.h>
+
+
+namespace Brittle
+{
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Forwards Declaration
+//
+
+// UI Components
+
+class Panel;
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+} // namespace Brittle
+
+#endif // __BRITTLE_UI_UI_TYPES_H
