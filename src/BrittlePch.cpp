a4255875533a361d357ce617c683e8633dcff6f3
slimek <slime573@gmail.com>
2015-02-04 22:56:23 +0800

    Upgrade to Cocos2d-x 3.4

--- src/BrittlePch.cpp
+++ src/BrittlePch.cpp
@@ -1,3 +1,10 @@
 // Brittle C++ Library - Precompiled Header Creator
 
 #include "BrittlePch.h"
+
+
+//
+// Validate Cocos2d-x version
+//
+
+static_assert( COCOS2D_VERSION == 0x00030400, "Cocos2d-x version not matched" );
