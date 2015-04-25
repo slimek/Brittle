8c3adf6e21f54c2505ce304ec8ff084343958274
slimek <slime573@gmail.com>
2015-04-25 21:55:31 +0800

    Change UiBuilder class names.
    - Do not use template style.
    - Add UiButtonBuilder::Text(), which may put a UiTextBuilder into it.

--- include/Brittle/Ui/UiBuilders.h
+++ include/Brittle/Ui/UiBuilders.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #include <Brittle/Setup/BrittleDefs.h>
+#include <Brittle/Layout/Align.h>
 
 
 namespace Brittle
@@ -12,48 +13,40 @@ namespace Brittle
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-// UI Builder Primitive Template
+// Builder for ui::Text
 //
 
-template< typename UiType >
-class UiBuilder;
-
-
-///////////////////////////////////////////////////////////////////////////////
-//
-// UI Builder for ui::Text
-//
-
-template<>
-class UiBuilder< ui::Text >
+class UiTextBuilder
 {
-    typedef UiBuilder< ui::Text > This;
-
 public:
 
-    UiBuilder()
+    UiTextBuilder()
         : m_text( ui::Text::create() )
     {}
 
-    explicit UiBuilder( const std::string& text )
+    explicit UiTextBuilder( const std::string& text )
         : m_text( ui::Text::create() )
     {
         m_text->setString( text );
     }
 
-    ui::Text* Build() const { return m_text; }
 
-    This& FontSize( Int fontSize )
+    ui::Text* Build()    const { return m_text; }
+    operator ui::Text*() const { return m_text; }
+
+
+    UiTextBuilder& FontSize( Int fontSize )
     {
         m_text->setFontSize( fontSize );
         return *this;
     }
 
-    This& Color( const Color4B& color )
+    UiTextBuilder& Color( const Color4B& color )
     {
         m_text->setTextColor( color );
         return *this;
     }
+
 
 private:
     ui::Text* m_text { nullptr };
@@ -62,35 +55,45 @@ private:
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-// UI Builder for ui::Button
+// Builder for ui::Button
 //
 
-template<>
-class UiBuilder< ui::Button >
+class UiButtonBuilder
 {
-    typedef UiBuilder< ui::Button > This;
-
 public:
 
-    UiBuilder( const std::string& normalImage )
+    UiButtonBuilder( const std::string& normalImage )
         : m_button( ui::Button::create( normalImage ))
     {}
 
-    ui::Button* Build() const { return m_button; }
 
-    This& PressedAction( Bool enabled )
+    ui::Button* Build()    const { return m_button; }
+    operator ui::Button*() const { return m_button; }
+
+
+    UiButtonBuilder& PressedAction( Bool enabled )
     {
         m_button->setPressedActionEnabled( enabled );
         return *this;
     }
 
-    template< typename Function, typename Target >
-    This& ClickEvent( const Function& func, const Target& target )
+    template< typename Function, typename Target, typename... Args >
+    UiButtonBuilder& ClickEvent( const Function& func, const Target& target, const Args&... args )
     {
         m_button->addClickEventListener(
-            std::bind( func, target, std::placeholders::_1 ));
+            std::bind( func, target, std::placeholders::_1, args... ));
         return *this;
     }
+
+    // Put a text at the center of the Button.
+    UiButtonBuilder& Text( ui::Text* text )
+    {
+        m_button->setCascadeOpacityEnabled( true );
+        m_button->addChild( text );
+        Locate( text ).Center();
+        return *this;
+    }
+
 
 private:
     ui::Button* m_button { nullptr };
